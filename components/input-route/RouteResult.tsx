287d1ad1c4db6cca95ae938f435e07ce62646f95
DanhVanPham <Danhskipper18@gmail.com>
2024-01-25 22:29:09 +0700

    Random Weather & Congestion

--- components/input-route/RouteResult.tsx
+++ components/input-route/RouteResult.tsx
@@ -1,23 +1,62 @@
-import React from "react";
-import { ResponseType } from "./InputRouteView";
-import { Divider, Stack } from "@mui/material";
-import CloudIcon from "@mui/icons-material/Cloud";
-import DirectionsBoatIcon from "@mui/icons-material/DirectionsBoat";
-import { addDays } from "@/utils/dateHelpers";
-import { KeyValueItem, RouteDate, RouteLine } from "./common";
+import React, { useMemo } from 'react'
+import { ResponseType } from './InputRouteView'
+import { Divider, Stack } from '@mui/material'
+import CloudIcon from '@mui/icons-material/Cloud'
+import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat'
+import { addDays } from '@/utils/dateHelpers'
+import { KeyValueItem, RouteDate, RouteLine } from './common'
+import { Congestion, CongestionEnum, Weather, WeatherEnum } from './constants'
+
+const randomWeather = () => {
+  const randNum = Math.round(Math.random() * 2)
+  return {
+    value: randNum,
+    label: (Weather as any)[randNum],
+  }
+}
+
+const randomCongestion = () => {
+  const randNum = Math.round(Math.random() * 2)
+  return {
+    value: randNum,
+    label: (Congestion as any)[randNum],
+  }
+}
+
+const BAD_WEATHER_DAYS = 10
+const BAD_CONGESTION_DAYS = 5
 
 function RouteResults({
   startDate,
   result,
 }: {
-  startDate: Date;
-  result: ResponseType;
+  startDate: Date
+  result: ResponseType
 }) {
-  const rangeDate = result?.["Estimate Time of Arrival (ETA) (days)"] || 0;
+  const transfData = useMemo(() => {
+    let rangeDate = result?.['Estimate Time of Arrival (ETA) (days)'] || 0
+    const weatherObj = randomWeather()
+    const congestionObj = randomCongestion()
+
+    if (weatherObj.value === WeatherEnum.Bad) {
+      rangeDate += BAD_WEATHER_DAYS
+    }
+
+    if (congestionObj.value === CongestionEnum.Bad) {
+      rangeDate += BAD_CONGESTION_DAYS
+    }
+
+    return {
+      rangeDate,
+      weather: weatherObj.label,
+      congestion: congestionObj.label,
+    }
+  }, [result])
+
   return (
     <Stack flex={1} width={1} spacing={2} my={3}>
       <Stack
-        direction={{ xs: "column", sm: "row" }}
+        direction={{ xs: 'column', sm: 'row' }}
         alignItems="stretch"
         sx={{
           p: 2,
@@ -30,14 +69,14 @@ function RouteResults({
           direction="row"
           spacing={1.5}
           sx={{
-            width: { xs: 1, sm: "70%", md: "60%" },
+            width: { xs: 1, sm: '70%', md: '60%' },
           }}
         >
           <RouteDate title="Origin" date={startDate.toLocaleDateString()} />
-          <RouteLine rangeDate={rangeDate} />
+          <RouteLine rangeDate={transfData.rangeDate} />
           <RouteDate
             title="Destination"
-            date={addDays(startDate, rangeDate).toLocaleDateString()}
+            date={addDays(startDate, transfData.rangeDate).toLocaleDateString()}
           />
         </Stack>
         <Stack>
@@ -46,7 +85,7 @@ function RouteResults({
         <Stack spacing={1}>
           <KeyValueItem
             propertyKey="Weather"
-            value="Good"
+            value={transfData.weather}
             icon={
               <CloudIcon
                 fontSize="small"
@@ -58,7 +97,7 @@ function RouteResults({
           />
           <KeyValueItem
             propertyKey="Congestion"
-            value="Normal"
+            value={transfData.congestion}
             icon={
               <DirectionsBoatIcon
                 fontSize="small"
@@ -71,7 +110,7 @@ function RouteResults({
         </Stack>
       </Stack>
     </Stack>
-  );
+  )
 }
 
-export default RouteResults;
+export default RouteResults
---
+++ components/input-route/constants.ts
@@ -0,0 +1,23 @@
+export enum WeatherEnum {
+  Bad = 0,
+  Normal = 1,
+  Good = 2,
+}
+
+export const Weather = {
+  [WeatherEnum.Bad]: 'Bad',
+  [WeatherEnum.Normal]: 'Normal',
+  [WeatherEnum.Good]: 'Good',
+}
+
+export enum CongestionEnum {
+  Bad = 0,
+  Normal = 1,
+  Good = 2,
+}
+
+export const Congestion = {
+  [CongestionEnum.Bad]: 'Bad',
+  [CongestionEnum.Normal]: 'Normal',
+  [CongestionEnum.Good]: 'Good',
+}
